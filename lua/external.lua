bab8e0045d7e6922fff67f753f20728fb80ec006
thgjone <thgjone@gmail.com>
2026-03-24 10:07:44 +0100

    /home/torhag/..path -> vim config

--- lua/external.lua
+++ lua/external.lua
@@ -1,5 +1,5 @@
 function _G.VimDML(choise)
-  local dmlfile = '$HOME/.config/nvim/vim.dml'
+  local dmlfile = vim.fn.stdpath 'config' .. '/nvim/vim.dml'
 
   -- 1. Temporarily set the variable in the current process
   vim.env.FT = vim.fn.expand '&filetype'
